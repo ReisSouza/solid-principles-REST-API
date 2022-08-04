f76aa2714eab6b7a3de53cc4d538aa43efe986e4
reissouza <reis.souza@outlook.com.br>
2022-08-04 17:50:45 -0300

    feat: :sparkles: add feature infor request  in terminal

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import morgan from 'morgan';
 import { pool } from './config/configDataBase/database';
-
 import { router } from './routes';
 
 const app = express();
 pool.connect();
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(router);
 export { app };
